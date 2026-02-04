6c975e99e290c4462b7c1f6ee2d551cc0291c52d
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2026-02-04 16:59:11 +0530

    project initialized

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require('./config/db.js');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "Backend is running and ready!" });
+});
+
+app.get('/db-test', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json({ 
+            status: "success", 
+            message: "Database is connected", 
+            time: result.rows[0].now 
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
